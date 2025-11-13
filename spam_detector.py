02748b4085b0290ce41888ead6ffda43df35d333
punithkv <151712386+punithkv@users.noreply.github.com>
2025-11-13 21:23:01 +0530

    Update CSV path to absolute location

--- spam_detector.py
+++ spam_detector.py
@@ -5,4 +5,5 @@ import pandas as pd
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 csv_path = os.path.join(BASE_DIR, "spam.csv")
 
-df = pd.read_csv(csv_path, encoding="latin-1")[['v1', 'v2']]
+df = pd.read_csv(r"C:\Users\punit\punith123\spam.csv", encoding="latin-1")[['v1', 'v2']]
+
