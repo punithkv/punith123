efaea7dcadd3fdf7e12bafb7ab4f3490e6872e5c
punithkv <151712386+punithkv@users.noreply.github.com>
2025-11-13 21:28:52 +0530

    Update CSV path to use dynamic base directory

--- spam_detector.py
+++ spam_detector.py
@@ -8,5 +8,5 @@ print(pd.read_csv(csv_path, encoding="latin-1").columns.tolist())
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 csv_path = os.path.join(BASE_DIR, "spam.csv")
 
-df = pd.read_csv(r"C:\Users\punit\punith123\spam.csv", encoding="latin-1")[['v1', 'v2']]
+df = pd.read_csv(csv_path, encoding="latin-1")[['v1', 'v2']]
 
